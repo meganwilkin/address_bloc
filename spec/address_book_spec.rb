2f8399d8a04cd67e1616e0654f616b9949f3d8f6
Megan Wilkin <megan.wilkin@blueostrichdesign.co.uk>
2017-04-09 15:41:31 +0100

    assignment remove entry test completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -17,5 +17,38 @@ require_relative '../models/address_book'
        expect(book.entries.size).to eq(0)
      end
    end
+   describe "#add_entry" do
+     it "adds only one entry to the address book" do
+       book = AddressBook.new
+       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
 
+       expect(book.entries.size).to eq(1)
+     end
+
+     it "adds the correct information to entries" do
+       book = AddressBook.new
+       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+       new_entry = book.entries[0]
+
+       expect(new_entry.name).to eq('Ada Lovelace')
+       expect(new_entry.phone_number).to eq('010.012.1815')
+       expect(new_entry.email).to eq('augusta.king@lovelace.com')
+     end
+   end
+
+   describe "#remove_entry" do
+     it "removes only one entry from the address book" do
+       book = AddressBook.new
+       book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+       expect(book.entries.size).to eq(0)
+     end
+
+     it "removes the correct information from entries" do
+       book = AddressBook.new
+       book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+       remove_entry = book.entries[0]
+
+    end
+  end
  end
